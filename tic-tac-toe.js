6db62f4a6cfc3734f81302fc13cb5c312dc98623
Kimani Allen <kimani2761469@hotmail.com>
2020-10-12 17:02:37 -0500

    Exercise 2 completed

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,8 +1,36 @@
 window.onload = function(event){
   let a = document.getElementById("board");
   let i = a.children;
+  const button = document.querySelector("button");
   var c ;
+  
   for (c=0 ; c < i.length; c++){
       i[c].setAttribute("class","square");
-    }
+  }
+   var n;
+  for (n=0; n < i.length; n++){
+    
+      i[n].onclick=function(event) {
+        let text = event.target.textContent;
+                
+    
+                if(text=="X"){
+                    event.target.classList.remove('X');
+                    event.target.classList.add('O');
+                    event.target.textContent='O';
+                }
+                else if(text == "O"){
+                    event.target.classList.remove('O');
+                    event.target.classList.add('X');
+                    event.target.textContent='X';
+                }
+                else{
+                    event.target.classList.add('X');
+                    event.target.textContent='X';
+                }
+            }   
+        }
 }
+
+    
+    
